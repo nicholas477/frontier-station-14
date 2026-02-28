960dac41b43a7d166673ff7982887f0764913cfa
EPICGameGuy <nicholas2.chalkley@gmail.com>
2026-02-28 00:49:57 -0500

    Workin on bounty

--- Content.Server/_EGG/BountyContracts/EGGBountyContractSystem.cs
+++ Content.Server/_EGG/BountyContracts/EGGBountyContractSystem.cs
@@ -1,4 +1,30 @@
 using Content.Server._NF.BountyContracts;
+using Content.Server._NF.Radio.Components;
+using Content.Shared._NF.BountyContracts;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server._EGG.BountyContracts;
 
+public sealed partial class EGGBountyContractSystem : EntitySystem
+{
+    [Dependency] private readonly IPrototypeManager _protoMan = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BountyContractsCartridgeComponent, GetBountyContractsEvent>(OnGetBountyContracts);
+    }
+
+    private void OnGetBountyContracts(EntityUid uid, BountyContractsCartridgeComponent component, GetBountyContractsEvent ev)
+    {
+        Log.Debug("OnGetBountyContracts");
+
+        ProtoId<BountyContractCollectionPrototype> antagCollection = "Antag";
+
+        if (ev.Collection == antagCollection)
+        {
+            ev.Bounties.Add(new BountyContract(1000, BountyContractCategory.Other, "test", 1000, GetNetEntity(uid), null, null, null, null));
+        }
+    }
+}
