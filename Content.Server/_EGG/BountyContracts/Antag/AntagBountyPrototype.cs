3008d1e3f961395c874d06e495fce9bbef65b89f
EPICGameGuy <nicholas2.chalkley@gmail.com>
2026-02-28 20:14:09 -0500

    Got antag bounties to show up in the pda

---
+++ Content.Server/_EGG/BountyContracts/Antag/AntagBountyPrototype.cs
@@ -0,0 +1,35 @@
+using Content.Shared._NF.BountyContracts;
+using Content.Shared.Objectives.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._EGG.BountyContracts.Antag;
+
+
+/// <summary>
+///     Collection of objectives
+/// </summary>
+[Prototype]
+public sealed partial class AntagBountyPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; private set; } = default!;
+
+    [DataField]
+    public ProtoId<BountyContractCollectionPrototype> Collection;
+
+    [DataField]
+    public string Name = "";
+
+    [DataField]
+    public string Description = "";
+
+    [DataField]
+    public int Reward = 0;
+
+
+    /// <summary>
+    /// List of static objectives to give.
+    /// </summary>
+    [DataField(required: true)]
+    public List<EntProtoId<ObjectiveComponent>> Objectives = new();
+}
--- Content.Server/_EGG/BountyContracts/EGGBountyContractSystem.cs
+++ Content.Server/_EGG/BountyContracts/EGGBountyContractSystem.cs
@@ -1,12 +1,22 @@
+using Content.Server._EGG.BountyContracts.Antag;
 using Content.Server._NF.BountyContracts;
 using Content.Server._NF.Radio.Components;
 using Content.Shared._NF.BountyContracts;
+using Content.Shared.BarSign;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.Mind;
+using JetBrains.FormatRipper.Elf;
 using Robust.Shared.Prototypes;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Content.Server._EGG.BountyContracts;
 
 public sealed partial class EGGBountyContractSystem : EntitySystem
 {
+    private readonly ProtoId<BountyContractCollectionPrototype> _antagCollection = "Antag";
+    //private readonly ProtoId<Entity> _objectivesCollection = "Antag";
+
+    [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly IPrototypeManager _protoMan = default!;
 
     public override void Initialize()
@@ -16,15 +26,54 @@ public sealed partial class EGGBountyContractSystem : EntitySystem
         SubscribeLocalEvent<BountyContractsCartridgeComponent, GetBountyContractsEvent>(OnGetBountyContracts);
     }
 
+    private bool GetMindFromCartridge(EntityUid uid, out EntityUid mindId, [NotNullWhen(true)] out MindComponent? mind)
+    {
+        TryComp<CartridgeComponent>(uid, out CartridgeComponent? comp);
+
+        var loaderEnt = comp?.LoaderUid;
+
+        // The entity holding this pda. 
+        EntityUid? pdaEnt = loaderEnt.HasValue ? Transform(loaderEnt.Value).ParentUid : null;
+        if (pdaEnt is null)
+        {
+            mindId = EntityUid.Invalid;
+            mind = null;
+            return false;
+        }
+
+        return _mind.TryGetMind(pdaEnt.Value, out mindId, out mind);
+    }
+
     private void OnGetBountyContracts(EntityUid uid, BountyContractsCartridgeComponent component, GetBountyContractsEvent ev)
     {
-        Log.Debug("OnGetBountyContracts");
-
-        ProtoId<BountyContractCollectionPrototype> antagCollection = "Antag";
-
-        if (ev.Collection == antagCollection)
+        if (!GetMindFromCartridge(uid, out var mindId, out var mind))
         {
-            ev.Bounties.Add(new BountyContract(1000, BountyContractCategory.Other, "test", 1000, GetNetEntity(uid), null, null, null, null));
+            //Log.Debug("Player mind: {0}", mind!.ToString());
+            return;
         }
+
+        // Iterate over all prototypes of a specific type
+        foreach (var prototype in _protoMan.EnumeratePrototypes<AntagBountyPrototype>())
+        {
+            // Do something with prototype
+            //Log.Info($"Prototype: {prototype.ID}");
+
+            if (prototype.Collection == ev.Collection)
+            {
+                ev.Bounties.Add(new BountyContract(1000, BountyContractCategory.Other, prototype.Name, prototype.Reward, GetNetEntity(uid), null, null, prototype.Description, null));
+            }
+        }
+
+
+
+
+        //// PDA
+        //var pdaParent = Transform(uid).ParentUid;
+        //var playerParent = Transform(pdaParent).ParentUid;
+
+        //if (ev.Collection == _antagCollection)
+        //{
+        //    ev.Bounties.Add(new BountyContract(1000, BountyContractCategory.Other, "test", 1000, GetNetEntity(uid), null, null, null, null));
+        //}
     }
 }
