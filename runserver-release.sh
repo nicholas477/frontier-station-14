f8eb746e2565a9142f589999b23a4afa6b9d5731
nicholas477 <nicholas2.chalkley@gmail.com>
2026-03-16 21:38:43 -0400

    Fixed up server config

--- runserver-release.sh
+++ runserver-release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dotnet run --project Content.Server --configuration Release --config-file=./Config/server_config_release.toml
+dotnet run --project Content.Server --configuration Release --config-file ./Config/server_config_release.toml
 read -p "Press enter to continue"
